0c0d573a039bd2d2f83bddccae8807876b28430d
Harsha G Math <harsha.gururaj.math@sap.com>
2026-05-29 15:14:30 +0530

    Update app

--- calci/app.py
+++ calci/app.py
@@ -1,13 +1,13 @@
-def add(a,b):
+def add(a,b): #Addition function
     return a + b
 
-def subtract(a,b):
+def subtract(a,b): #Subtraction function
     return a-b
 
-def multiply(a,b):
+def multiply(a,b): #Multiplication function
     return a*b
 
-def divide(a,b):
+def divide(a,b): #Division function
     if b == 0:
         return "Division by 0 is not possible."
     return a/b
